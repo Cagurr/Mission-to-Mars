53f225c31e87f7eaecf660ef7a5eb649ad575add
Chris Gurr <cagurr.cg@gmail.com>
2022-08-30 17:12:28 -0400

    Adding Challenge files thru D2

--- scraping.py
+++ scraping.py
@@ -3,8 +3,7 @@ from splinter import Browser
 from bs4 import BeautifulSoup as soup
 from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
-
-def scrape_all():
+import datetime as dt
 
 def scrape_all():
     # Initiate headless driver for deployment
@@ -12,6 +11,7 @@ def scrape_all():
     browser = Browser('chrome', **executable_path, headless=True)
 
     news_title, news_paragraph = mars_news(browser)
+    hemisphere_image_urls = hemispheres(browser)
 
     # Run all scraping functions and store results in dictionary
     data = {
@@ -19,9 +19,10 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
-        "last_modified": dt.datetime.now()
+        "last_modified": dt.datetime.now(),
+        "hemispheres": hemisphere_image_urls
     }
-
+    
     # Stop webdriver and return data
     browser.quit()
     return data
@@ -98,6 +99,48 @@ def mars_facts():
 
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html(classes="table table-striped")
+
+def hemispheres(browser):
+    # # D1: Scrape High-Resolution Mars’ Hemisphere Images and Titles
+    # Hemispheres
+    # 1. Use browser to visit the URL 
+    url = 'https://marshemispheres.com/'
+
+    browser.visit(url)
+
+    # 2. Create a list to hold the images and titles.
+    hemisphere_image_urls = []
+    hemis = ['cerberus', 'schiaparelli', 'syrtis_major', 'valles_marineris']
+
+    # 3. Write code to retrieve the image urls and titles for each hemisphere
+    for i in range(0,4):
+        hemisphere = {}
+
+        # find first link to the full images
+        full_image_elem = browser.find_by_tag('h3')[i]
+        full_image_elem.click()
+
+        # Parse the resulting html with soup
+        html = browser.html
+        img_soup = soup(html, 'html.parser')
+        
+        # Find the image urls
+        img_url = f'https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/' + hemis[i] + '_enhanced.tif/full.jpg'
+        
+        # Find the title of the image
+        img_title = img_soup.select_one('h2', class_='title').text
+
+        # Add url and title to hemisphere dict
+        hemisphere = {'img_url': img_url, 'img_title': img_title}
+        
+        # Append the list with a dictionary of the url and title
+        hemisphere_image_urls.append(hemisphere)
+
+        # back up 1 page
+        browser.back()
+
+    # Return statment
+    return hemisphere_image_urls
 
 if __name__ == "__main__":
     # If running as script, print scraped data
